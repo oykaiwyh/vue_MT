2c2eea722d49b93538cdcfa5ce6227cddb46469b
yangkun <1641250803@qq.com>
2019-11-08 17:33:53 +0800

    passport update yang

--- server/app/interface/utils/passport.js
+++ server/app/interface/utils/passport.js
@@ -1,13 +1,23 @@
 import passport from "egg-passport";
 import  LocalStrategy from "egg-passport-local";
-import UserModel from "../../model/user";
 
-passport.use(new LocalStrategy(function(username, password, done){
-    const { ctx } = this;
-    let who ={
-        username
-    }
-    let result = await UserModel.findOne(who)
-}))
+module.exports = app =>{
+    const { ctx } = app
+    app.passport.use(new LocalStrategy(function(username, password, done){
+        const { ctx } = this;
+        let who ={
+            username
+        }
+        let result = await ctx.model.UserModel.findOne(who)
+        if( result != null ){
+            if(result.passport=== passport){
+                return done(null,result)
+            }else{
+                return done( null , false , '密码错误') 
+            }
+        }
+    }))
+}
+
 
 
